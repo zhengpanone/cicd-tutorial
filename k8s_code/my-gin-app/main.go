1dd2f4e9f74cab78c3f6d806bea335bbd0366235
zhengpanone <zhengpanone@hotmail.com>
2026-06-26 17:18:26 +0800

    my gin code

--- k8s_code/my-gin-app/main.go
+++ k8s_code/my-gin-app/main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-var version = flag.String("v", "v1", "v1")
+var (
+	version = flag.String("v", "v1", "server version")
+	port    = flag.Int("p", 8080, "server port")
+)
 
 func main() {
+	flag.Parse()
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		flag.Parse()
+
 		hostname, _ := os.Hostname()
 		c.String(200, "Version: %s, Hostname: %s", *version, hostname)
 	})
@@ -22,11 +28,19 @@ func main() {
 		flag.Parse()
 		hostname, _ := os.Hostname()
 		c.JSON(http.StatusOK, gin.H{
-			"path":     c.Param("path"),
 			"version":  *version,
 			"hostname": hostname,
 		})
 	})
 
-	router.Run(":8080")
+	router.Run(fmt.Sprintf(":%d", getPort()))
+}
+
+func getPort() int {
+	if p := os.Getenv("PORT"); p != "" {
+		if port, err := strconv.Atoi(p); err == nil {
+			return port
+		}
+	}
+	return *port
 }
